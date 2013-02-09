ceae824ddc6c5267aa08cf705178fbb7ed32bc6c
Daisuke Fujiwara <daisuke@nextdoor.com>
2013-02-09 12:51:16 -0800

    Initial commit.

---
+++ HealthHack/HealthHack/HealthAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HealthAppDelegate.h
+//  HealthHack
+//
+//  Created by Daisuke Fujiwara on 2/9/13.
+//  Copyright (c) 2013 dfujiwara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HealthAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HealthHack/HealthHack/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HealthHack
+//
+//  Created by Daisuke Fujiwara on 2/9/13.
+//  Copyright (c) 2013 dfujiwara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HealthAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HealthAppDelegate class]));
+    }
+}
